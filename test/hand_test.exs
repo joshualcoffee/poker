4b041dc8fbd0ff5018e1137cfcaa1714d03608f0
Josh Coffee <joshualcoffee@gmail.com>
2019-10-10 08:18:09 -0400

    add test to implement one pair

--- test/hand_test.exs
+++ test/hand_test.exs
@@ -6,5 +6,9 @@ defmodule Poker.CardTest do
     test "for high card" do
       :high_card = Hand.play("4H 6C 7H 3D QH")
     end
+
+    test "for one pair" do
+      :one_pair = Hand.play("4H 4D 6C 7H 3D")
+    end
   end
 end

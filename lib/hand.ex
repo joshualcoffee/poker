d8302d69dd0079275e98c883a07c37db5046ddd2
Josh Coffee <joshualcoffee@gmail.com>
2019-10-10 08:16:58 -0400

    implement high card functionality. This will be the default as it's the last option for the hand.

--- lib/hand.ex
+++ lib/hand.ex
@@ -1,10 +1,27 @@
 defmodule Poker.Hand do
+  alias Poker.Card
   defstruct cards: [], suits: %{}, values: %{}
 
   @type t :: %__MODULE__{
           cards: List.t(Card.t())
         }
-
+  @spec play(String.t()) :: atom()
   def play(cards) do
+    cards = convert(cards)
+
+    cond do
+      true ->
+        :high_card
+    end
+  end
+
+  @spec convert(String.t()) :: __MODULE__.t()
+  defp convert(cards) do
+    cards =
+      cards
+      |> String.split(" ")
+      |> Enum.map(&Card.convert(&1))
+
+    %__MODULE__{cards: cards}
   end
 end

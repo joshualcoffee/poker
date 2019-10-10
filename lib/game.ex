b49eb20dcf6b52635fdb0c4e297e24ef12adbac5
Josh Coffee <joshualcoffee@gmail.com>
2019-10-10 11:42:13 -0400

    testing for ties

--- lib/game.ex
+++ lib/game.ex
@@ -3,7 +3,8 @@ defmodule Poker.Game do
   Module responsible for playing a single round of poker.
   """
   alias Poker.Hand
-
+  alias Poker.Card
+  @spec play_round(String.t(), String.t()) :: atom()
   def play_round(set1, set2) do
     player_1_hand = Hand.play(set1)
     player_2_hand = Hand.play(set2)
@@ -16,7 +17,33 @@ defmodule Poker.Game do
         :player_2
 
       true ->
-        :no_winner
+        tie_breaker(player_1_hand, player_2_hand)
     end
+  end
+
+  defp tie_breaker(%{hand: :high_card, cards: player_1_cards}, %{
+         cards: player_2_cards
+       }) do
+    if (player_1_cards |> List.last()).value > (player_2_cards |> List.last()).value do
+      :player_1
+    else
+      :player_2
+    end
+  end
+
+  defp tie_breaker(player_1_hand, player_2_hand)
+       when player_1_hand.hand in [:one_pair] do
+    hand
+    |> pair_sum
+  end
+
+  @spec pairs(__MODULE__.t()) :: List.t()
+  defp pair_sum(hand) do
+    hand.values
+    |> Map.values()
+    |> Enum.filter(fn val -> length(val) == 2 end)
+    |> List.flatten()
+    |> Enum.map(& &1.value)
+    |> Enum.sum()
   end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -16,9 +16,13 @@ defmodule Poker.GameTest do
     end
   end
 
-  describe "tieing Poker.Game.play_round" do
-    test "when players tie" do
-      assert_winner(:no_winner, Game.play_round("4H 6C TH TD QH", "4H 6C 9H 9D QH"))
+  describe "tieing :high_card Poker.Game.play_round" do
+    test "when players tie and player 1 has the higer card" do
+      assert_winner(:player_1, Game.play_round("4H 6C 9H TD KH", "4H 6C 7H 8D QH"))
+    end
+
+    test "when players tie and player 2 has the higer card" do
+      assert_winner(:player_2, Game.play_round("4H 6C 9H TD KH", "4H 6C 7H 8D AH"))
     end
   end
 end
